d757722e84060b2d4ad2c6f7fdde60ed80bacfd6
Caleb Stultz <kalub92@gmail.com>
2016-03-04 00:22:00 +0700

    Initial Commit

---
+++ my-hood-devslopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-hood-devslopes
+//
+//  Created by Caleb Stultz on 3/4/16.
+//  Copyright © 2016 Caleb Stultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
